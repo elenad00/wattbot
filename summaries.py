70cd8f1751d732066ac61a46327b224f5a8c7e92
elena <ejday00@gmail.com>
2021-12-23 16:07:28 +0000

    adding error checking

--- summaries.py
+++ summaries.py
@@ -13,29 +13,52 @@ def getyesterday(bookid):
 from pymongo import MongoClient
 from datetime import date, timedelta
 from sys import argv
+from plotter import plot
 
 <<<<<<< HEAD
 def getyesterday(bookid, db):
 >>>>>>> 1ea88e4 (refined adding new works)
 =======
 def getyesterday(books, db):
+<<<<<<< HEAD
 >>>>>>> 368042a (added weekly and monthly summaries)
     print("[*] Retrieving todays data")
     getdata(2, books, db, 1)
+=======
+    print("[*] Comparing todays data")
+    data = getdata(2, books, db)
+>>>>>>> bcae140 (adding error checking)
 
-def getdata(x, books, db, y):
+def getthisweek(books, db):
+    print("[*] Retrieving this weeks data")
+    data = getdata(7, books, db)
+
+def getthismonth(books, db):
+    print("[*] Retrieving this months data")
+    data = getdata(28, books, db)
+
+def getdata(x, books, db):
     days = []
-    for i in range(0,x,y):
+    for i in range(0,x):
         days.append((date.today() - timedelta(days=i)).strftime("%d%m%Y"))
+
     for book in books:
         res = []
         bookid = book['id']
         for day in days:
             res.append(getvals(day, bookid, db))
-        if (len(res[0]) and len(res[1]))>1:
-            compare(res, y)
-        else:
-            print("[!] Not enough data available")
+        compdata = [res[0][0], res[0][1]-res[-1][1], res[0][2]-res[-1][2], res[0][3]-res[-1][3], res[0][4]-res[-1][4], res[0][5]-res[-1][5], round(res[0][6]-res[-1][6],1)]
+        compare(compdata)
+        reads,likes,chapters,alpc,rpc,isc = [], [],[],[],[],[]
+        for i in range(0,x):
+            reads.append(res[i][1])
+            likes.append(res[i][2])
+            chapters.append(res[i][3])
+            alpc.append(res[i][4])
+            rpc.append(res[i][5])
+            isc.append(res[i][6])
+        plot([reads,likes,chapters,alpc,rpc,isc])
+
 
 def getvals(date, bookid, db):
     data = db["instances"].find({'date':date, 'bookid':bookid}, {'_id':0, 'date':0})
@@ -49,25 +72,8 @@ def getvals(date, bookid, db):
         print(e)
         exit(1)
 
-def compare(data, y):
-    compdata = []
-    for i in range(0, len(data)-1):
-        val = []
-        for z in range(0, len(data[i])):
-            if z != 0:
-                val.append(round(data[i][z]-data[i+1][z],1))
-            else:
-                val.append(data[i][z])
-        compdata.append(val)
-        print(f'[+] Value Increases for book {compdata[i][0]}:\n\tReads: {compdata[i][1]}\n\tLikes: {compdata[i][2]}\n\tChapters: {compdata[i][3]}')
-        print(f'\tAverage Likes Per Chapter Change: {compdata[i][4]}')
-        print(f"\tReads per Like Change: {compdata[i][5]}")
-        print(f"\tInteraction Score Change: {compdata[i][6]}\n")
-
-def getthisweek(books, db):
-    print("[*] Retrieving this weeks data")
-    getdata(7, books, db, 6)
-
-def getthismonth(books, db):
-    print("[*] Retrieving this months data")
-    getdata(28, books, db, 27)
+def compare(compdata):
+    print(f'[+] Value Increases for book {compdata[0]}:\n\tReads: {compdata[1]}\n\tLikes: {compdata[2]}\n\tChapters: {compdata[3]}')
+    print(f'\tAverage Likes Per Chapter Change: {compdata[4]}')
+    print(f"\tReads per Like Change: {compdata[5]}")
+    print(f"\tInteraction Score Change: {compdata[6]}\n")
