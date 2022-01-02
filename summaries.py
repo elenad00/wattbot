315b21a4aeca41496056d4b171dde5e9aa84522d
elena <elenajeanday@gmail.com>
2022-01-02 23:21:43 +0000

    added requirements.txt

--- summaries.py
+++ summaries.py
@@ -25,10 +25,18 @@ def getdata(x, books, db):
         res = []
         bookid = book['id']
         for day in days:
-            res.append(getvals(day, bookid, db))
+            val = getvals(day, bookid, db)
+            while not val:
+                if day == '01012022':
+                    day = '31122021'
+                else:
+                    day = int(day) - 1000000
+                    val = getvals(day, bookid, db)
+            res.append(val)
+
         compdata = [res[0][0], res[0][1]-res[-1][1], res[0][2]-res[-1][2], res[0][3]-res[-1][3], round(res[0][4]-res[-1][4], 1), res[0][5]-res[-1][5], round(res[0][6]-res[-1][6],1)]
         compare(compdata)
-        reads,likes,chapters,alpc,rpc,isc = [], [],[],[],[],[]
+        reads,likes,chapters,alpc,rpc,isc = [],[],[],[],[],[]
         for i in range(0,x):
             reads.append(res[i][1])
             likes.append(res[i][2])
@@ -40,16 +48,14 @@ def getdata(x, books, db):
 
 
 def getvals(date, bookid, db):
-    data = db["instances"].find({'date':date, 'bookid':bookid}, {'_id':0, 'date':0})
+    data = db["instances"].find({'date':str(date), 'bookid':bookid}, {'_id':0, 'date':0})
     ints=''
     for instance in data:
         ints = list(instance.values())
-    try:
+    if len(ints)>0:
         return ints
-    except Exception as e:
-        print('[!] Error querying MongoDB')
-        print(e)
-        exit(1)
+    else:
+        return False
 
 def compare(compdata):
     print(f'[+] Value Increases for book {compdata[0]}:\n\tReads: {compdata[1]}\n\tLikes: {compdata[2]}\n\tChapters: {compdata[3]}')
